f1b8b06a98d05133a73ce879fe7a8bc4ba4a5835
Shirshak <shirshak.acharya@gmail.com>
2025-03-05 14:31:30 +0545

    Day_7

--- Day_7/784_letter_case_permutation.py
+++ Day_7/784_letter_case_permutation.py
@@ -22,9 +22,27 @@ def letter_case_permutation(string_S):
 
     return outputs
 
+# Recursive Approach 
+# Time O(2^n) & Space O(N+ 2^n)
+def letter_case_permutation_recursive(string_S):
+    res = []
+    def back_tracking(sub_str="", i=0):
+        if len(sub_str) == len(string_S):
+            res.append(sub_str)
+            return # get out if substring == string_S and append to main list
+        
+        if string_S[i].isalpha():
+            back_tracking(sub_str+string_S[i].swapcase(), i+1) # recursive until the len(sub_str) == len(string_S) for each of upper and lower string and each numbers. 
+
+        back_tracking(sub_str+string_S[i], i+1)
+
+    back_tracking()
+    return res
+
 
 
 if __name__=="__main__":
     string_S = 'a1b1'
     print(letter_case_permutation(string_S))
+    print(letter_case_permutation_recursive(string_S))
 
