c93f0ac629808f4299f5fe58c6f2f8f21f4a14af
Shirshak <shirshak.acharya@gmail.com>
2025-03-05 22:42:24 +0545

    Day_7

--- Day_7/46_permutations.py
+++ Day_7/46_permutations.py
@@ -34,8 +34,28 @@ def permutations(nums):
     return outputs
 
 
+# nums=[1,2,3], at first, perms=[[]] and p_copy is going to append 1 at 1st, 2nd and 3rd location which is [[1]] itself, so perms=new_perms=[[1]]. 
+# At n=2, in outer for loop, then, n=2 will be kept in 3 location from 0 to range(len(p)+1)=3, hence perms=new_perms=[[2,1], [1,2]], 
+# and 2nd location and 3rd location is same in this case. and params=new_params=[[2,1], [1,2]]
+# At n=3 in outer loop , for p=[2,1] in perms for i=0 to 3, insert number 3 at 3 locations, [3,2,1][2,3,1][2,1,3]. Similarly for p=[1,2] in perms, for i=0 to 3, insert number 3 at 3 locations, [3,1,2], [1,3,2], [1,2,3]
+
+def permutations_iterative(nums):
+    perms = [[]]
+
+    for n in nums:
+        new_perms = []
+        for p in perms:
+            for i in range(len(p)+1):
+                p_copy = p[:]
+                p_copy.insert(i, n)
+                new_perms.append(p_copy)
+
+        perms = new_perms
+    return perms
+
 
 if __name__=="__main__":
     nums = [1,2,3]
     print(permutations(nums))
+    print(permutations_iterative(nums))
 
