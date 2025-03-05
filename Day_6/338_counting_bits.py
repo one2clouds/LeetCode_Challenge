87b8576dcb97a7ca1090440a40903f8f48a26eb9
Shirshak <shirshak.acharya@gmail.com>
2025-03-05 11:58:27 +0545

    Day_6

--- Day_6/338_counting_bits.py
+++ Day_6/338_counting_bits.py
@@ -1,13 +1,18 @@
 
+# time complexity = O(N)
 def countBits(num):
     dp = [0] * (n+1)
     offset = 1
 
-
+    # for example if we have i=8, then previously our offset was at 4, i.e 
+    # no. of bits become 1 again as 4 = 100, current as 4*2 = 8, so offset 
+    # becomes 8 and number of bits for  becomes 1 again and increases until 
+    # next offset i.e 16
+    
     for i in range(num+1):
         if offset*2 == i:
             offset =i 
-        dp[i] = 1+ dp[i-offset]
+        dp[i] = 1+ dp[i-offset] # 
         
     return dp 
 
