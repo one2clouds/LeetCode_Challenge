5e2531a8c8849a199913d004b29881e9aa34ea27
Shirshak <shirshak.acharya@gmail.com>
2025-03-08 21:02:04 +0545

    Day_10

--- Day_10/2379_min_recolors_to_get_k_consecutive_black.py
+++ Day_10/2379_min_recolors_to_get_k_consecutive_black.py
@@ -1,5 +1,6 @@
 # trivial and bruteforce solution
-# TIme COmplexity : O(N*k), O(N) for the N strings on while loop, O(k) for k substrings 
+# TIme COmplexity : O(N*k), O(N) for the N letter on while loop, O(k) for k substrings 
+# Space Complexity : O(1) since only left, right, min_num_color_change, and num_color_change are needed
 def minimum_recolors_for_black(my_string, k):
     left, right = 0, 0
     min_num_color_change = k 
@@ -21,11 +22,39 @@ def minimum_recolors_for_black(my_string, k):
     return min_num_color_change
 
 
+# Better Solution
+# TIme COmplexity : O(N), O(N) for the N letter on loop
+# Space Complexity : O(1) since only left, right, min_num_color_change, and num_color_change are needed
+def minimum_recolors_for_black_2(my_string, k):
+    left = 0
+    min_num_color_change = k
+    num_color_change = 0
+
+    # go from 0 to end of string 
+    for right in range(len(my_string)):
+        # for each index update change color to black if found White
+        if my_string[right] == "W":
+            num_color_change += 1
+
+        # at the k index, check minimum color change as len(my_string)<k, and leftmost value if white then do -1, because we are about to go to next index to check the color
+        if right - left+1 == k:
+            min_num_color_change = min(num_color_change, min_num_color_change)
+            if my_string[left] == "W":
+                num_color_change -= 1
+            left += 1
+
+    return min_num_color_change
+        
+
+
 if __name__=="__main__":
     my_string = "WBBWWBBWBW"
     k = 7
     print(minimum_recolors_for_black(my_string, k))
+    print(minimum_recolors_for_black_2(my_string, k))
+
 
     my_string = "WBWBBBW"
     k = 2
     print(minimum_recolors_for_black(my_string, k))
+    print(minimum_recolors_for_black_2(my_string, k))
