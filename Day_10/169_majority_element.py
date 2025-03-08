3550441a95c8068f5f6404e796238a39bf5339de
Shirshak <shirshak.acharya@gmail.com>
2025-03-08 23:40:10 +0545

    Day_10

--- Day_10/169_majority_element.py
+++ Day_10/169_majority_element.py
@@ -19,12 +19,27 @@ def majority_element(nums):
     return max_value
 
 
+# Space complexity  O(1)     # better for space 
+def majority_element_2(nums):
+    count, max_value = 0, 0
+    for num in nums: 
+        if count == 0:
+            max_value = num 
+
+        # check if the max_value is our number, add 1 if it is, add -1 if it isn't
+        count += (1 if max_value == num else -1)
+
+    return max_value
+        
+
 
 if __name__ == "__main__":
     nums = [3,2,3]
 
     print(majority_element(nums))
+    print(majority_element_2(nums))
 
     nums = [2,2,1,1,1,2,2]
     print(majority_element(nums))
+    print(majority_element_2(nums))
 
