bf917e946c9b3ce81695a87353e9f6779d90663d
Shirshak <shirshak.acharya@gmail.com>
2025-03-08 22:28:22 +0545

    Day_10

--- Day_10/25_remove_duplicate_from_sorted_array.py
+++ Day_10/25_remove_duplicate_from_sorted_array.py
@@ -3,11 +3,13 @@
 def remove_duplicates(nums):
     left = 1
 
+    # set left to 1, increase right from by 1 to len(nums)      
     for right in range(1, len(nums)):
+        # current item, not equal to previous....
         if nums[right] != nums[right-1]:
+            # at location of left pointer value put item from right pointer
             nums[left] = nums[right]
             left += 1
-
     return left
         
 
